45e66819969eb68c5943241e6c9210627fc15967
cmcnr82 <cmcnr82@gmail.com>
2024-06-25 08:49:07 +0300

    first commit

---
+++ src/main/java/com/company/microservice/controller/controller.java
@@ -0,0 +1,17 @@
+package com.company.microservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class controller {
+
+    @GetMapping("/")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello from Railway + Spring!");
+    }
+
+    @GetMapping("/bye")
+    public ResponseEntity<String> bye() {
+        return ResponseEntity.ok("Bye...");
+    }
+}
